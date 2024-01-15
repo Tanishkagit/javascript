7f549b51b44ef13b529f97798979459d6a867598
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-15 11:59:27 +0530

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,16 @@
+//create a const object called "product" and store information.
+const product ={
+    productName:"Shirt",
+    productPrice: 500,
+    productRating:5
+}
+console.log(product);
+//create a const object ccalled "profile" to store information.
+const profile={
+    userName: "Tanishka Kamthariya",
+    followers: 128,
+    following: 171,
+    isFollow: true,
+    bio: "nothing"
+}
+console.log(profile);
