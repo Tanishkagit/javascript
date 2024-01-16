c28709aec7ddb974dc84e28fa4a83ea133ab36de
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-16 14:36:14 +0530

    Update temlit.js

--- temlit.js
+++ temlit.js
@@ -2,3 +2,19 @@
 let userName=prompt("enter name: ");
 let fullName=`@${userName}${userName.length}`;
 console.log(fullName);
+
+
+//use string uppercase method
+let userName=prompt("enter name: ");
+let fullName=`@${userName}${userName.length}`; // we use camel case to represent variable name
+console.log(fullName);
+let newName= `${fullName.toUpperCase()}`;
+console.log(newName);
+
+//use string lowercase method
+let userName=prompt("enter name: ");
+let fullName=`@${userName.toUpperCase()}${userName.length}`;
+console.log(fullName);
+let newName= `${fullName.toLowerCase()}`;
+console.log(newName);
+
