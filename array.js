bdead2b1cc05a2f03ee5070056d4f7a3ad5003b4
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-17 12:40:38 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,21 @@
+//array queation to calculate average of marks.
+let array=[85,97,44,37,76,60];
+let arr;
+let sum=0;
+let avg;
+for(arr of array){
+    console.log(arr);
+    sum=sum+arr;
+}
+console.log(sum);
+avg=sum/array.length;
+console.log(avg);
+
+//array queation related to array methods.
+let companies=["bloomberg","microsoft","uber","google","IBM","netflix"];
+companies.shift();
+console.log(companies);
+companies.splice(1,1,"ola");
+console.log(companies);
+companies.push("amazon");
+console.log(companies);
