8e8d1f0cdfc34292a2466af73415974e7532475e
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-18 23:43:16 +0530

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,18 @@
+//for loop for odd number.
+let i;
+for(i=0;i<=500;i++){
+    if(i%2 !==0){
+    console.log(i);
+    }
+}
+
+//for loop for even number.
+let i;
+for(i=0;i<=500;i++){
+    if(i%2 ===0){   
+    console.log(i);
+    }
+}
+
+
+
