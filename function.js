6daff8db4e1f899c80b89713b8f28843b378fcba
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-20 21:53:10 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,24 @@
+//print your message by using function.
+function myfunction(){
+    console.log("I am learning javascript");
+}
+myfunction();
+
+//print message by using parameter and argument.
+function message(msg){
+    console.log(msg);
+}
+message("I LOVE YOU MY SPECIAL ONE"); 
+
+//create a function for sum of two numbers.
+function sum(x,y){
+    console.log(x+y);
+}
+sum(9,8);
+
+//create a function for multiply of two numbers
+function mul(x,y){
+    console.log(x*y);
+}
+mul(9,8);
+
