e5e7373ce245e7b99bfc0d92f111af73801a333e
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-21 22:34:00 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 //array queation to calculate average of marks.
-let array=[85,97,44,37,76,60];
+let array= [85,97,44,37,76,60];
 let arr;
 let sum=0;
 let avg;
@@ -12,7 +12,7 @@ avg=sum/array.length;
 console.log(avg);
 
 //array queation related to array methods.
-let companies=["bloomberg","microsoft","uber","google","IBM","netflix"];
+let companies= ["bloomberg","microsoft","uber","google","IBM","netflix"];
 companies.shift();
 console.log(companies);
 companies.splice(1,1,"ola");
