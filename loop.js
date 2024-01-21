5a2d880545a73239d30c268d21d627d423c3db7d
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-21 22:23:10 +0530

    Update loop.js

--- loop.js
+++ loop.js
@@ -14,5 +14,37 @@ for(i=0;i<=500;i++){
     }
 }
 
+//while loop
+let i = 1;
+while(i <= 5){
+    console.log("Hi JavaScript");
+    i++;
+}
+
+//While loop for even number.
+let i = 0;
+while (i <= 100){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+    i++;
+}
+
+//while loop for odd number.
+let i = 0;
+while (i <= 100){
+    if(i % 2 !=== 0){
+        console.log(i);
+    }
+    i++;
+}
+
+// Do while loop.
+let i = 10;
+do{
+    console.log("Hello JavaScript");
+    i++;
+}while(i <= 5);
+
 
 
