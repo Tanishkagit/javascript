b99d6ee693cde4a486ea5a4819c1c73ab25d37d8
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-02-15 22:02:19 +0530

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -4,3 +4,13 @@ for(i=1;i<=4;i++){
     fact=fact*i;
 }
 console.log(fact);
+
+//using function
+function fact(){
+    let fact=1;
+    let i;
+    for(i=1;i<=10;i++){
+        fact=fact*i;
+    }
+    console.log(fact);
+}
