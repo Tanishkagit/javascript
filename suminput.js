c47ade3be51b5eaa4c13a98febc3d6b110816b5b
Tanishkagit <tanishkak1803@gmail.com>
2024-02-18 14:30:09 +0530

    suminput.js

---
+++ suminput.js
@@ -0,0 +1,14 @@
+//sum of two values.
+let value1=parseInt(prompt("enter value: "));
+let value2=parseInt(prompt("enter value: "));
+let finalevalue=value1+value2;
+console.log(finalevalue);
+
+//sum of two values using function.
+function sum(){
+let value1=parseInt(prompt("enter value: "));
+let value2=parseInt(prompt("enter value: "));
+let finalevalue=value1+value2;
+console.log(finalevalue);
+}
+sum();
