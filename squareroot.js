40842f5e1cc8db8b447578eb64fbbbe8dfbc8080
Tanishkagit <tanishkak1803@gmail.com>
2024-02-18 13:23:58 +0530

    squareroot.js

---
+++ squareroot.js
@@ -0,0 +1,14 @@
+//JavaScript Program to Find the Square.
+function findsquareroot(){
+    let number=parseInt(prompt("enter number: "));
+    let newnumber=number*number;
+    console.log(newnumber);
+}
+findsquareroot();
+//JavaScript Program to Find the Square Root using method.
+function findsquareroot(){
+    let number=parseInt(prompt("enter number: "));
+    let newnumber= Math.sqrt(number);
+    console.log(newnumber);
+}
+findsquareroot();
