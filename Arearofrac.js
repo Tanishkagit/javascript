33757ffd405f2d86736f4d511cdd5f96982b660a
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-02-19 21:52:31 +0530

    Create Arearofrac.js

---
+++ Arearofrac.js
@@ -0,0 +1,4 @@
+ let lenght=parseInt(prompt("enter the lenght of ractangle:- "));
+let width=parseInt(prompt("enter the width of ractangle:- "));
+ let areaofractangle=lenght*width;
+console.log(areaofractangle);
