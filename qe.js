802f3b263fcb739d51b8acda633a2178734a402e
Tanishkagit <tanishkak1803@gmail.com>
2024-02-23 20:24:59 +0530

    quadratic equation

--- qe.js
+++ qe.js
@@ -3,7 +3,18 @@ let a=parseInt(prompt("enter value: "));
 let b=parseInt(prompt("enter value: "));
 let c=parseInt(prompt("enter value: "));
 let value= b*b - 4*a*c;
-formula1=(-b + Math.sqrt(value))/2;
-formula1=(-b - Math.sqrt(value))/2;
-console.log(formula1);
-console.log(formula2);
+if(value>0){
+    formula1= (-b + Math.sqrt(value))/2*a;
+    formula2= (-b - Math.sqrt(value))/2*a;
+console.log(`${formula1}, ${formula2}`);
+}
+else if(value==0){
+    formula1= (-b)/2*a;
+    formula2= (-b)/2*a;
+    console.log(`${formula1}, ${formula2}`);
+}
+else{
+    let rp = (-b / (2 * a)).toFixed(2);
+    let ip = (Math.sqrt(-value) / (2 * a)).toFixed(2);
+    console.log(`${rp}, ${ip}`);
+}
