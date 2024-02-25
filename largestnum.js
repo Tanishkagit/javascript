e4b85b4af9807ae41b0fe225dc1a15bca1c5b831
Tanishkagit <tanishkak1803@gmail.com>
2024-02-25 14:08:36 +0530

    largestnumber.js

---
+++ largestnum.js
@@ -0,0 +1,12 @@
+let num1=parseInt(prompt("enter number: "));
+let num2=parseInt(prompt("enter number: "));
+let num3=parseInt(prompt("enter number: "));
+if(num1>num2){
+    console.log(`the largest number is ${num1}`);
+}
+else if(num2>num3){
+    console.log(`the largest number is ${num2}`);
+}
+else{
+    console.log(`the largest number is ${num3}`);
+}
