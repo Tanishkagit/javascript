c364e1d19ba5d33cc3437028086f31825447965b
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-03-02 19:57:54 +0530

    Update Class.js

--- Class.js
+++ Class.js
@@ -1,3 +1,4 @@
+//example1.
 class pen{
 full(){
 console.log("pen rifill is full");
@@ -7,4 +8,27 @@ console.log("pen rifill is empty");
 }
 }
 let bluepen= new pen();
+
+//example2.
+class students{
+  information(){
+    name=prompt("enter student name: ");
+    age=parseInt(prompt("enter student age: "));
+    emailId=prompt("enter student emailid: ");
+    phoneNum=parseInt(prompt("enter student phonenumber: "));
+    console.log(name);
+    console.log(age);
+    console.log(emailId);
+    console.log(phoneNum);
+  }
+marks(){
+  marks=parseInt(prompt("enter student marks: "));
+console.log(marks);
+}
+}
+let info= new information();
+let mark= new marks();
+
+
+
 
