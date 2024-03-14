adf4df1eeae6adfb1f81645a7aacc0b634ea32bb
Tanishkagit <tanishkak1803@gmail.com>
2024-03-14 21:05:00 +0530

    randomnumber.js

--- randomnum.js
+++ randomnum.js
@@ -1,2 +1,12 @@
+let ownNumber=parseFloat(prompt("enter value: "));
+console.log(ownNumber);
 let randomNumber=Math.random();
 console.log(randomNumber);
+if(randomNumber>ownNumber){
+    console.log("random number is greater then your number");
+}
+else if(randomNumber===ownNumber){
+    console.log("random number is equal then your number");
+} else{
+    console.log("random number is less then your number");
+}
