40ed84502af9c1c78df63852ed1d23827e0568ba
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-03-29 21:22:12 +0530

    Update Class.js

--- Class.js
+++ Class.js
@@ -1,10 +1,10 @@
 //example1.
 class pen{
 full(){
-console.log("pen rifill is full");
+console.log("pen rifill is full.");
 }
 empty(){
-console.log("pen rifill is empty");
+console.log("pen rifill is empty.");
 }
 }
 let bluepen= new pen();
