e2f70b9ec2cfbb4f4772723dd1c20e92e84a07de
Tanishkagit <tanishkak1803@gmail.com>
2024-04-21 12:13:20 +0530

    classes

---
+++ classes.js
@@ -0,0 +1,54 @@
+//example1......
+class pen{
+    full(){
+    console.log("pen rifill is full.");
+    }
+    empty(){
+    console.log("pen rifill is empty.");
+    }
+    }
+    let bluepen= new pen();
+    bluepen.full();
+    bluepen.empty();
+    
+    //example2....
+    class students{
+      information(){
+        name=prompt("enter student name: ");
+        age=parseInt(prompt("enter student age: "));
+        emailId=prompt("enter student emailid: ");
+        phoneNum=parseInt(prompt("enter student phonenumber: "));
+        console.log(name);
+        console.log(age);
+        console.log(emailId);
+        console.log(phoneNum);
+      }
+    marks(){
+      marks=parseInt(prompt("enter student marks: "));
+    console.log(marks);
+    }
+    }
+    let info= new students();
+    info.information();
+     info.marks();
+    
+    
+    //example.
+    //constructor is automatically created when you create a class.
+    class game{
+      constructor(){
+        
+        console.log("this is a constructor")
+      }
+    start(){
+      
+    console.log("your game is start");
+    }
+    end(){
+      
+    console.log("your game is end");
+    }
+    }
+    let ludo= new game();
+    
+    //these are the examples of classes..
