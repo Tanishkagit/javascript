0a769cd133105285a78d5d395e5498bc209fb1ae
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-05-13 22:23:46 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,5 +1,6 @@
 function add(x,y){
     console.log(x+y);
+    console.log("hello");
 }
 function cal(x,y,add){
     add(x,y);
@@ -9,5 +10,6 @@ cal(3,6,add);
 //by using arrow function.
 const hiii=()=>{
     console.log("hii");
+    console.log("hello");
 }
 setTimeout(hiii,4000)
