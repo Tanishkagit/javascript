0f8f8cc39af5d0af36008ec7e587ffb6c8d035a6
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-07-11 21:31:41 +0530

    Create Click.js

---
+++ Click.js
@@ -0,0 +1,8 @@
+let menubtn= document.getElement("NAME");
+let closebtn= document.getElement("NAME");
+menubtn.addEventListner('clicked',()=>{
+name.style.display= "flex";
+});
+closebtn.addEventListner('clicked',()=>{
+name.style.display= "none";
+});
